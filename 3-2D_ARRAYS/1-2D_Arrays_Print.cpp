c250ab58427847bf192b6451e741e8ce405c5b35
David_Sahu <65307635+DsAizen@users.noreply.github.com>
2021-06-05 23:28:51 +0530

    Create 1-2D_Arrays_Print.cpp

---
+++ 3-2D_ARRAYS/1-2D_Arrays_Print.cpp
@@ -0,0 +1,23 @@
+
+//2d Array
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+    int a[100][100];
+    int m,n;
+    cin>>m>>n;
+    int val=1;
+    for(int row=0; row<m; row++){
+        for(int col=0; col<n; col++){
+            a[row][col]=val;
+             val++;
+            cout<<a[row][col]<<" ";
+           
+        }cout<<endl;
+    }
+
+    return 0;
+}
