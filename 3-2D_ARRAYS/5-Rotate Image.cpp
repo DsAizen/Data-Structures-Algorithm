ceb0d89e07d33363c61b8c5df8b24087e7efceb5
David_Sahu <65307635+DsAizen@users.noreply.github.com>
2021-06-18 22:23:35 +0530

    Update 5-Rotate Image.cpp

--- 3-2D_ARRAYS/5-Rotate Image.cpp
+++ 3-2D_ARRAYS/5-Rotate Image.cpp
@@ -1,5 +1,16 @@
 
-//Rotate image(colock wise)
+/*Rotate image(colock wise)
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]] */
 #include <iostream>
 
 using namespace std;
