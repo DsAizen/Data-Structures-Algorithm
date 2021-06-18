4b3fd55858f00f5a09e37e5ab986c526d7de2420
David_Sahu <65307635+DsAizen@users.noreply.github.com>
2021-06-18 22:21:04 +0530

    Update 5-Rotate Image.cpp

--- 3-2D_ARRAYS/5-Rotate Image.cpp
+++ 3-2D_ARRAYS/5-Rotate Image.cpp
@@ -1,1 +1,30 @@
 
+//Rotate image(colock wise)
+#include <iostream>
+
+using namespace std;
+void rotate_image(int a[100][100], int m, int n){
+    for(int col=0; col<n; col++){
+        for(int row=n-1; row>=0; row--){
+            cout<<a[row][col]<<" ";
+        }cout<<endl;
+    }
+}
+int main()
+{	
+    int a[100][100];
+    int m,n;
+    cin>>m>>n;
+    int val=1;
+    for(int row=0; row<m; row++){
+        for(int col=0; col<n; col++){
+            a[row][col]=val;
+             val++;
+            cout<<a[row][col]<<" ";
+           
+        }cout<<endl;
+    }
+    cout<<"----------------------"<<endl;
+    rotate_image(a,m,n);
+    return 0;
+}
