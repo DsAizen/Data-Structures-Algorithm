e0ab4c7f3312393f9988fd3523888e1c7f37e056
David_Sahu <65307635+DsAizen@users.noreply.github.com>
2021-06-26 21:00:55 +0530

    Create 7- 2D Caracter Array.cpp

---
+++ 3-2D_ARRAYS/7- 2D Caracter Array.cpp
@@ -0,0 +1,8 @@
+/*how to work with string in 2D array
+white spaces- any cracter randers with space(normal space-fix ascii 32)
+ char b[] [10] ={{'d','a','v','\0'},{'i','d','\0'};
+ char c[] [10] ={"david","kumar","sahu"};              no need '\0' termination
+ cout<<b[1];                 o/p-a
+ cout<<c[0];                 o/p-david
+ 
+ */
