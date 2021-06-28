95290ae2d4db695645b718ea9469245d78c7e5f6
David_Sahu <65307635+DsAizen@users.noreply.github.com>
2021-06-28 12:01:47 +0530

    Update 1- Character Arrays Intro.cpp

--- 4- Character Arrayes/1- Character Arrays Intro.cpp
+++ 4- Character Arrayes/1- Character Arrays Intro.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[]={1,2,3,4};   //address of an array
+    
+    char b[10]={'a','b','c'}
+    cin>>b;                 //content of the array+garbage value (5)
+    
+    char c[]={'x','y','z','\0'};
+                                //content (6)
+    cout<<b<<" "<<sizeof(b);
+    return 0;
+}
