b19d03e7745eb63df0a3c24ecbbd111d9eb4c602
David_Sahu <65307635+DsAizen@users.noreply.github.com>
2021-07-24 21:28:42 +0530

    Update 1- find sum.cpp

--- 8- Algorithms STL/1- find sum.cpp
+++ 8- Algorithms STL/1- find sum.cpp
@@ -1,2 +1,24 @@
 /*The header algorithm defines a collection of functions especially designed to be used on ranges of elements.
 <algorithm> */
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {2,5,1,9};
+    int n = sizeof(arr)/sizeof(int);
+    int key = 5;
+    
+    auto search = find(arr, arr+n,key);
+    int index = search - arr;
+    cout<<index;
+    
+    
+
+    return 0;
+}
+
