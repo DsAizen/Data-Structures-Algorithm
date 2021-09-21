7c3adfa9548480fe4c34990ae3ed42b95ad75378
David_Sahu <65307635+DsAizen@users.noreply.github.com>
2021-09-21 23:22:20 +0530

    Update 2- binary search.cpp

--- 8- Algorithms STL/2- binary search.cpp
+++ 8- Algorithms STL/2- binary search.cpp
@@ -1,46 +1,26 @@
 //binary search
 
-#include <iostream>
 
-using namespace std;
-int binary_search(int a[], int n, int key){
-int s=0;
-int e=n-1;
-while(s<=e){
-int mid=(s+e)/2;
-if(a[mid]==key){
-return mid;
-}else if(a[mid]<key){
-s=mid+1;
-}else{
-e=mid-1;
-}
-}
-return -1;
-}
-
-int main(){
-        int a[100];
        
-        int n;
-        cout<<"enter n =";
-        cin>>n;
-         
-        
-         cout<<"enter array =";
-        
-        for(int i=0; i<n; i++){
-            cin>>a[i];
-        }
-        int key;
-         cout<<"enter key =";
-         cin>>key;
-         
-    
-    cout<<"key index="<<binary_search(a, n, key);
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+
+    int key=2;
+    int a[]={1,2,3,4,5};
+    int n= sizeof(a)/sizeof(int);
+    
+    bool present = binary_search(a,a+n,key);
+    if(present){
+        cout<<"found";
+    }else{
+        cout<<"not found";
+    }
     return 0;
 }
+
 ///////////////////////////////////////////////////////////
 
 #include <iostream>
